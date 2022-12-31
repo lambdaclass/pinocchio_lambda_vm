e3c9e6a9c89e6819f43007807ee8b92c69a7850f
MauroFab <maurotoscano2@gmail.com>
2022-12-30 23:29:56 -0300

    Extracted C paramaters from proof

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod math;
 mod pinocchio;
 
 use circuits::qap::Qap;
+use math::field_element::FieldElement as FE;
 use pinocchio::prover;
 use pinocchio::setup::{setup, ToxicWaste};
 
@@ -10,7 +11,24 @@ fn main() {
     let test_qap = Qap::new_test_qap();
     let toxic_waste = ToxicWaste::sample();
     let (evaluation_key, verifying_key) = setup(&test_qap, &toxic_waste);
-    let proof = prover::generate_proof(&evaluation_key, &test_qap);
+
+    let inputs = [
+        FE::new(3).unwrap(),
+        FE::new(3).unwrap(),
+        FE::new(3).unwrap(),
+        FE::new(3).unwrap(),
+    ];
+
+    // For the test circuit c_mid and c_output has only one element
+    let (c_mid, c_output) = Qap::test_qap_solver(inputs);
+
+    let mut c_vector = vec![FE::zero()];
+    c_vector.append(&mut inputs.to_vec());
+    c_vector.push(c_mid);
+    c_vector.push(c_output);
+
+    let proof = prover::generate_proof(&evaluation_key, &test_qap, &c_vector);
+
     println!("Evaluation key: {:?}", evaluation_key);
     println!("Verifying key: {:?}", verifying_key);
     println!("Proof: {:?}", proof);
--- src/pinocchio/prover.rs
+++ src/pinocchio/prover.rs
@@ -30,37 +30,31 @@ pub fn msm(c: &[FE], hidings: &[GroupType]) -> Option<GroupType> {
         .reduce(|acc, x| acc + x)
 }
 
-pub fn generate_proof(evaluation_key: &EvaluationKey, qap: &Qap) -> Proof {
+/// Generates a proof from an evaluation_key,
+/// the qap representation of the circuit
+/// and the C of the C0, the inputs, the intermediate results and the final results
+pub fn generate_proof(
+    evaluation_key: &EvaluationKey,
+    qap: &Qap,
+    qap_c_coefficients: &[FE],
+) -> Proof {
     //Inputs + solution should be an argument
-    let inputs = [
-        FE::new(2).unwrap(),
-        FE::new(2).unwrap(),
-        FE::new(2).unwrap(),
-        FE::new(2).unwrap(),
-    ];
 
-    let (c5, c6) = Qap::test_qap_solver(inputs);
+    let c_mid = &qap_c_coefficients
+        [qap.number_of_inputs + 1..(qap_c_coefficients.len() - qap.number_of_outputs)];
 
-    let c_mid = vec![c5];
-    let c_output = vec![c6];
-
-    let mut c_vector = vec![FE::zero()];
-    c_vector.extend(inputs);
-    c_vector.extend(&c_mid);
-    c_vector.extend(c_output);
-
-    let h_polynomial = qap.h_polynomial(&c_vector);
+    let h_polynomial = qap.h_polynomial(qap_c_coefficients);
 
     // If C and the evaluation key are valid, this shouldn't fail
     // TO DO: Raise an error if some value can't be computated
     Proof {
-        g_v_s: msm(&c_mid, &evaluation_key.gv_ks).unwrap(),
-        g_ws: msm(&c_mid, &evaluation_key.gw_ks).unwrap(),
-        g_ys: msm(&c_mid, &evaluation_key.gy_ks).unwrap(),
-        g_alpha_vs: msm(&c_mid, &evaluation_key.gv_alphaks).unwrap(),
-        g_alpha_ws: msm(&c_mid, &evaluation_key.gw_alphaks).unwrap(),
-        g_alpha_ys: msm(&c_mid, &evaluation_key.gy_alphaks).unwrap(),
-        g_beta_vwy: msm(&c_mid, &evaluation_key.g_beta).unwrap(),
+        g_v_s: msm(c_mid, &evaluation_key.gv_ks).unwrap(),
+        g_ws: msm(c_mid, &evaluation_key.gw_ks).unwrap(),
+        g_ys: msm(c_mid, &evaluation_key.gy_ks).unwrap(),
+        g_alpha_vs: msm(c_mid, &evaluation_key.gv_alphaks).unwrap(),
+        g_alpha_ws: msm(c_mid, &evaluation_key.gw_alphaks).unwrap(),
+        g_alpha_ys: msm(c_mid, &evaluation_key.gy_alphaks).unwrap(),
+        g_beta_vwy: msm(c_mid, &evaluation_key.g_beta).unwrap(),
         // evaluation key may have extra elements, since in this implementation
         // we used an upper bound of elements
         g_hs: msm(
