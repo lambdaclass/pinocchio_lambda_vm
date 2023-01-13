a2c68a3d9af787751a6063a3d6661456187228fe
MauroFab <maurotoscano2@gmail.com>
2023-01-13 10:52:30 -0300

    A,B,C as R1CS generation

--- arkworks_adapter/src/lib.rs
+++ arkworks_adapter/src/lib.rs
@@ -3,70 +3,77 @@ use ark_ff::{BigInteger256, Fp256, FpParameters, PrimeField};
 use ark_relations::r1cs::ConstraintSystemRef;
 use num_bigint::BigUint;
 use pinocchio_vm::circuits::r1cs::R1CS;
-use pinocchio_vm::math::field_element::FieldElement ;
-
+use pinocchio_vm::math::field_element::FieldElement;
 
 type FE = FieldElement<5>;
 
 pub fn arcworks_cs_to_pinocchio_r1cs(cs: &ConstraintSystemRef<Fp256<FqParameters>>) {
     cs.inline_all_lcs();
 
-    let a: Vec<(String, usize)> = cs.to_matrices().unwrap().a[0]
-        .iter()
-        .map(|(x, y)| (x.to_string(), *y))
-        .collect();
-    let b: Vec<(String, usize)> = cs.to_matrices().unwrap().b[0]
-        .iter()
-        .map(|(x, y)| (x.to_string(), *y))
-        .collect();
-    let c: Vec<(String, usize)> = cs.to_matrices().unwrap().c[0]
-        .iter()
-        .map(|(x, y)| (x.to_string(), *y))
-        .collect();
+    let r1cs_matrices = cs.to_matrices().unwrap();
 
-    println!("Mod: {:?}", FqParameters::MODULUS.to_string());
-    println!("R: {:?}", FqParameters::R.to_string());
-    println!("Inv: {:?}", FqParameters::INV.to_string());
+    println!("CS Wit vars: {:?}", cs.num_witness_variables());
+    println!("CS Inst vars: {:?}", cs.num_instance_variables());
 
+    let A = arcworks_r1cs_matrix_to_pinocchio_r1cs_matrix(
+        &r1cs_matrices.a,
+        cs.num_instance_variables(),
+    );
 
-    println!("CS A: {:?}", a);
-    println!("CS B: {:?}", b);
-    println!("CS B: {:?}", c);
+    let B = arcworks_r1cs_matrix_to_pinocchio_r1cs_matrix(
+        &r1cs_matrices.b,
+        cs.num_instance_variables(),
+    );
 
-    let r1cs_matrixes = cs.to_matrices().unwrap();
-
-    let a = sparse_arcworks_matrix_fp_to_u128(&r1cs_matrixes.a);
-    let b = sparse_arcworks_matrix_fp_to_u128(&r1cs_matrixes.b);
-    let c = sparse_arcworks_matrix_fp_to_u128(&r1cs_matrixes.c);
-
-    println!("Converted CS A: {:?}", a);
-    println!("Converted CS B: {:?}", b);
-    println!("Converted CS B: {:?}", c);
+    let C = arcworks_r1cs_matrix_to_pinocchio_r1cs_matrix(
+        &r1cs_matrices.c,
+        cs.num_instance_variables(),
+    );
 }
 
-fn sparse_arcworks_matrix_fp_to_pinocchio_fe (m: &Vec<Vec<(Fp256<FqParameters>,usize)>>) -> Vec<Vec<(FE, usize)>> 
-    {
-    m.iter().map( |x| {
-        x.iter()
-        .map( |(x, y)| {
-            (biguint_to_u128(x.into_repr().into()),*y)
+fn arcworks_r1cs_matrix_to_pinocchio_r1cs_matrix(
+    m: &Vec<Vec<(Fp256<FqParameters>, usize)>>,
+    num_instance_vars: usize,
+) -> Vec<Vec<FE>> {
+    sparse_matrix_to_dense(&arcworks_matrix_fps_to_pinocchio_fes(m), num_instance_vars)
+}
+
+fn arcworks_matrix_fps_to_pinocchio_fes(
+    m: &Vec<Vec<(Fp256<FqParameters>, usize)>>,
+) -> Vec<Vec<(FE, usize)>> {
+    m.iter()
+        .map(|x| {
+            x.iter()
+                .map(|(x, y)| (biguint_to_u128(x.into_repr().into()), *y))
+                .map(|(x, y)| (FE::new(x), y))
+                .collect()
         })
-        .map ( |x, y| (FE::new(x),y))
         .collect()
-    }).collect()
 }
 
+fn sparse_matrix_to_dense(m: &Vec<Vec<(FE, usize)>>, num_ins_vars: usize) -> Vec<Vec<FE>> {
+    m.iter()
+        .map(|row| sparse_row_to_dense(row, num_ins_vars))
+        .collect()
+}
+
+fn sparse_row_to_dense(row: &Vec<(FE, usize)>, num_instance_vars: usize) -> Vec<FE> {
+    //The first column of the r1cs is used for constants
+    // TO DO: Check constants usage
+    let mut dense_row = vec![FE::new(0); num_instance_vars + 1];
+    for element in row {
+        dense_row[element.1] = element.0;
+    }
+    dense_row
+}
 
 /// Converts biguint to u128
 /// If the biguint is bigger than u128 it takes the first 128 bytes
 fn biguint_to_u128(big: BigUint) -> u128 {
-
     match big.to_u64_digits().len() {
         0 => 0,
         1 => big.to_u64_digits()[0].into(),
-        _ => 
-            big.to_u64_digits()[0] as u128 
-            & ((big.to_u64_digits()[1] as u128) << 64)
+        _ => big.to_u64_digits()[0] as u128 & ((big.to_u64_digits()[1] as u128) << 64),
     }
 }
 
